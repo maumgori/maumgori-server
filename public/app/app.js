96690a03e8e0657d77e159829be396b1cbeb303e
Jongmin Kim <kimjmin@gmail.com>
2015-03-05 16:58:16 +0900

    스켈레톤 얼추 완성.

---
+++ public/app/app.js
@@ -0,0 +1,13 @@
+var app = angular.module('app', []);
+
+app.directive('main',function(){
+  return {
+    restrict: 'E',
+    templateUrl: '/conts/main',
+    controller: 'mainCtrl'
+  }
+});
+
+app.controller('mainCtrl', function($scope) {
+  $scope.myVar = "Hello Angular";
+});
